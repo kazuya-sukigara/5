8493efb1b7fdb6b9afd43526ede4c586cb4c94fd
kazuya-sukigara <kazuyahahaha@icloud.com>
2020-07-20 06:16:56 +0000

    root&コントローラー

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,37 @@
 class GroupsController < ApplicationController
+	before_action :set_group, only: [:edit, :update]
+
+	def index # ここを追加
+    end
+
 	def new
     @group = Group.new
     @group.users << current_user
-  end
-
-  def create
-    @group = Group.new(group_params)
-    if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
-    else
-      render :new
     end
-  end
 
-  private
-  def group_params
-    params.require(:group).permit(:name, user_ids: [] )
-  end
+    def create
+	    @group = Group.new(group_params)
+	    if @group.save
+	      redirect_to root_path, notice: 'グループを作成しました'
+	    else
+	      render :new
+	    end
+    end
+
+    def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+    end
+
+	private
+	def group_params
+	 params.require(:group).permit(:name, user_ids: [] )
+	end
+
+	def set_group # ここ3
+    @group = Group.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-  
+  root 'groups#index'
+  resources :groups, only: [:new, :create, :edit, :update]
   resources :users,only: [:show,:index,:edit,:update] do
   get :search, on: :collection
   member do
