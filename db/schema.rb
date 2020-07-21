4c1113a4e7794038ecdd1adc50d6cac1ef9bfe05
kazuya-sukigara <kazuyahahaha@icloud.com>
2020-07-21 11:06:54 +0000

    タグmodel

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_20_060923) do
+ActiveRecord::Schema.define(version: 2020_07_21_110423) do
+
+  create_table "article_tag_relations", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_article_tag_relations_on_article_id"
+    t.index ["tag_id"], name: "index_article_tag_relations_on_tag_id"
+  end
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -93,6 +109,12 @@ ActiveRecord::Schema.define(version: 2020_07_20_060923) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
